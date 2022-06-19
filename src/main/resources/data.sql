bc3abaf93ed3d6d726aeb589dd5a7dcb0cda93a8
moo-cow <devjy0309@gmail.com>
2022-06-19 19:26:24 +0900

    add data.sql -> Change the initial entry method

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, 'Pororo', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (2, 'Lupi', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (3, 'Crong', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (4, 'Penguin', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (5, '손흥민', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (6, '호날두', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (7, '김제베', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (8, '유재석', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (9, '강호동', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (10, '홍길동', now(), now());
